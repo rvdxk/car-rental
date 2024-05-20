d105b7a59db9a55de9f9123fe2ca9274302040d3
rvdxk <radek.szczot@gmail.com>
2024-05-20 23:16:22 +0200

    added rental mapper

---
+++ src/main/java/io/github/rvdxk/carrentalspringproject/mapper/RentalMapper.java
@@ -0,0 +1,31 @@
+package io.github.rvdxk.carrentalspringproject.mapper;
+
+import io.github.rvdxk.carrentalspringproject.dto.RentalDto;
+import io.github.rvdxk.carrentalspringproject.entity.Rental;
+
+public class RentalMapper {
+
+    public static RentalDto mapToRentalDto(Rental rental) {
+        RentalDto rentalDto = new RentalDto(
+                rental.getId(),
+                rental.getCustomerId(),
+                rental.getCarId(),
+                rental.getRentalDate(),
+                rental.getReturnDate(),
+                rental.getTotalCost(),
+        );
+        return rentalDto;
+    }
+
+    public static Rental mapToRental(RentalDto rentalDto) {
+        Rental rental = new Rental(
+                rentalDto.getId(),
+                rentalDto.getCustomerId(),
+                rentalDto.getCarId(),
+                rentalDto.getRentalDate(),
+                rentalDto.getReturnDate(),
+                rentalDto.getTotalCost()
+        );
+        return rental;
+    }
+}
