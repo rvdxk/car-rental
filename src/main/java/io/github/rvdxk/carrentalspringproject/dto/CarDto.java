8e6f98a98dcacede76cdbd8a73ef8d71301ed28e
rvdxk <radek.szczot@gmail.com>
2024-05-20 23:17:01 +0200

    added car dto impl

---
+++ src/main/java/io/github/rvdxk/carrentalspringproject/dto/CarDto.java
@@ -0,0 +1,18 @@
+package io.github.rvdxk.carrentalspringproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CarDto {
+
+    private Long id;
+    private String make;
+    private String model;
+    private String plateNumber;
+    private int costPerHour;
+    private boolean isAvailable;
+}
