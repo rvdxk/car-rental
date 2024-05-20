922abdbc1e8261f74eff397bd73cad1ccd757c2d
rvdxk <radek.szczot@gmail.com>
2024-05-20 22:02:36 +0200

    added rental class

---
+++ src/main/java/io/github/rvdxk/carrentalspringproject/entity/Rental.java
@@ -0,0 +1,27 @@
+package io.github.rvdxk.carrentalspringproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "rental_information")
+public class Rental {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private Long customerId;
+    @Column(nullable = false)
+    private LocalDate rentalDate;
+    @Column(nullable = false)
+    private LocalDate returnDate;
+    @Column(nullable = false)
+    private double totalCost;
+}
