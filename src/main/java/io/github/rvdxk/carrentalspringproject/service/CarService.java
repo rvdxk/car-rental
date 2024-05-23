72443b4824ba43ec396cdc6d9a5747565075ff74
rvdxk <radek.szczot@gmail.com>
2024-05-24 00:15:52 +0200

    added car service layer

--- src/main/java/io/github/rvdxk/carrentalspringproject/service/CarService.java
+++ src/main/java/io/github/rvdxk/carrentalspringproject/service/CarService.java
@@ -1,17 +1,20 @@
 package io.github.rvdxk.carrentalspringproject.service;
 
 import io.github.rvdxk.carrentalspringproject.dto.CarDto;
-import io.github.rvdxk.carrentalspringproject.entity.CarParam;
+import io.github.rvdxk.carrentalspringproject.entity.CarParams;
+import io.github.rvdxk.carrentalspringproject.exception.ResourceNotFoundException;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface CarService {
     List<CarDto> getAllCars();
+    List<CarDto> getRentedCars(LocalDate rentalDate, LocalDate returnDate);
     List<CarDto> getAvailableCars(LocalDate rentalDate, LocalDate returnDate);
     void addCar(CarDto carDto);
-    CarDto getCarById(Long carId);
-    void updateCar(CarDto carDto, Long carId);
-    void updateCarParam(CarParam carParam, Long carParamId);
-    void deleteCar(Long carId);
+//    void addCarParams(Long id, CarParams carParams);
+    CarDto getCarById(Long id);
+    void updateCar(Long id, CarDto carDto);
+    void updateCarParam(Long id, CarParams carParams);
+    void deleteCar(Long id);
 }
--- src/main/java/io/github/rvdxk/carrentalspringproject/service/impl/CarServiceImpl.java
+++ src/main/java/io/github/rvdxk/carrentalspringproject/service/impl/CarServiceImpl.java
@@ -2,11 +2,11 @@ package io.github.rvdxk.carrentalspringproject.service.impl;
 
 import io.github.rvdxk.carrentalspringproject.dto.CarDto;
 import io.github.rvdxk.carrentalspringproject.entity.Car;
-import io.github.rvdxk.carrentalspringproject.entity.CarParam;
+import io.github.rvdxk.carrentalspringproject.entity.CarParams;
 import io.github.rvdxk.carrentalspringproject.entity.Rental;
 import io.github.rvdxk.carrentalspringproject.exception.ResourceNotFoundException;
 import io.github.rvdxk.carrentalspringproject.mapper.CarMapper;
-import io.github.rvdxk.carrentalspringproject.repository.CarParamRepository;
+import io.github.rvdxk.carrentalspringproject.repository.CarParamsRepository;
 import io.github.rvdxk.carrentalspringproject.repository.CarRepository;
 import io.github.rvdxk.carrentalspringproject.repository.RentalRepository;
 import io.github.rvdxk.carrentalspringproject.service.CarService;
@@ -23,27 +23,27 @@ public class CarServiceImpl implements CarService {
     @Autowired
     private CarRepository carRepository;
     @Autowired
-    private CarParamRepository carParamRepository;
+    private CarParamsRepository carParamsRepository;
     @Autowired
     private RentalRepository rentalRepository;
 
-    public CarServiceImpl(CarRepository carRepository, CarParamRepository carParamRepository, RentalRepository rentalRepository) {
+    public CarServiceImpl(CarRepository carRepository, CarParamsRepository carParamsRepository, RentalRepository rentalRepository) {
         this.carRepository = carRepository;
-        this.carParamRepository = carParamRepository;
+        this.carParamsRepository = carParamsRepository;
         this.rentalRepository = rentalRepository;
     }
 
     @Override
     public List<CarDto> getAllCars() {
-        List<Car> carList = carRepository.findAll();
-        List<CarDto> carDtoList = carList.stream().
+        List<Car> carsList = carRepository.findAll();
+        List<CarDto> carsDtoList = carsList.stream().
                 map((car) -> CarMapper.mapToCarDto(car))
-                .collect(Collectors.toUnmodifiableList());
-        return carDtoList;
+                .toList();
+        return carsDtoList;
     }
 
     @Override
-    public List<CarDto> getAvailableCars(LocalDate rentalDate, LocalDate returnDate) {
+    public List<CarDto> getRentedCars(LocalDate rentalDate, LocalDate returnDate) {
         List<Car> carList = carRepository.findAll();
         List<Rental> rentedRentals = rentalRepository.findByReturnDateIsNullOrReturnDateAfter(LocalDate.now());
         List<Car> rentedCars = rentedRentals.stream()
@@ -54,46 +54,94 @@ public class CarServiceImpl implements CarService {
                 .collect(Collectors.toUnmodifiableList());
 
         return rentedCarsDto;
+    }
 
+    @Override
+    public List<CarDto> getAvailableCars(LocalDate rentalDate, LocalDate returnDate) {
+        List<Car> allCars = carRepository.findAll();
+        List<Rental> rentedCars = rentalRepository.findByReturnDateIsNullAndRentalDateBetween(rentalDate, returnDate);
 
+        List<Car> rentedCarEntities = rentedCars.stream()
+                .map(Rental::getCar)
+                .collect(Collectors.toList());
+        List<CarDto> availableCars = allCars.stream()
+                .map(car -> {
+                    boolean isAvailable = !false;
+                    car.setAvailable(isAvailable);
+                    return CarMapper.mapToCarDto(car);
+                })
+                .filter(carDto -> !carDto.isAvailable())
+                .collect(Collectors.toList());
+
+        if (availableCars.isEmpty()) {
+            throw new ResourceNotFoundException("Sorry, we don't have any available cars for the specified dates.");
+        }
+
+        return availableCars;
+    }
+
+    @Override
+    public CarDto getCarById(Long id) {
+        Car car = carRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Car with id " + id + " not found"));
+        CarDto carDto = CarMapper.mapToCarDto(car);
+        return carDto;
     }
 
     @Override
     public void addCar(CarDto carDto) {
         Car car = CarMapper.mapToCar(carDto);
         carRepository.save(car);
+    }
 
+//    @Override
+//    public void addCarParams(Long id, CarParams carParams) {
+//        Car car = carRepository.findById(id)
+//                .orElseThrow(()-> new ResourceNotFoundException("Car with id " + id + " not found"));
+//        if (car.getCarParams() != null){
+//            carParamsRepository.delete(car.getCarParams());
+//        }
+//        carParams.setId(car.getId());
+//        carParams.setCar(car);
+//        car.setCarParams(carParams);
+//
+//        carParamsRepository.save(carParams);
+//        carRepository.save(car);
+//    }
+
+    @Override
+    public void updateCar(Long id, CarDto carDto) {
+        Car car = carRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Car with id " + id + " not found"));
+        if(car.getCarParams() != null) {
+
+            carParamsRepository.delete(car.getCarParams());
+        }
+        Car updatedCar = CarMapper.mapToCar(carDto);
+        updatedCar.setId(car.getId());
+        carRepository.save(updatedCar);
     }
 
     @Override
-    public CarDto getCarById(Long carId) {
-        Car car = carRepository.findById(carId)
-                .orElseThrow(() -> new ResourceNotFoundException("Car with id " + carId + " not found"));
-        CarDto carDto = CarMapper.mapToCarDto(car);
-        return carDto;
+    public void updateCarParam(Long id, CarParams carParams) {
+        Car car = carRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Car with id " + id + " not found"));
+        if(car.getCarParams() != null) {
+
+            carParamsRepository.delete(car.getCarParams());
+        }
+        car.setId(car.getId());
+        carParams.setId(car.getId());
+        car.setCarParams(carParams);
+        carParamsRepository.save(carParams);
+        carRepository.save(car);
     }
 
     @Override
-    public void updateCar(CarDto carDto, Long carId) {
-        Car car = carRepository.findById(carId)
-                .orElseThrow(()-> new ResourceNotFoundException("Car with id " + carId + " not found"));
+    public void deleteCar(Long id) {
+        Car car = carRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Car with id " + id + " not found"));
 
-        carRepository.save(CarMapper.mapToCar(carDto));
-    }
-
-    @Override
-    public void updateCarParam(CarParam carParam, Long carId) {
-        CarParam updateCarParam = carParamRepository.findById(carId)
-                .orElseThrow(()-> new ResourceNotFoundException("Car with id " + carId + " not found"));
-
-        carParamRepository.save(updateCarParam);
-    }
-
-    @Override
-    public void deleteCar(Long carId) {
-        Car car = carRepository.findById(carId)
-                .orElseThrow(()-> new ResourceNotFoundException("Car with id " + carId + " not found"));
-
-        carRepository.deleteById(carId);
+        carRepository.deleteById(id);
     }
 }
