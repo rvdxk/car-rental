d66dd8e3874a30b85a5555e9caa18ad931d8c096
rvdxk <radek.szczot@gmail.com>
2024-06-03 00:04:09 +0200

    delete class and refactor

--- src/main/java/io/github/rvdxk/carrentalspringproject/repository/RentalRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.rvdxk.carrentalspringproject.repository;
-
-import io.github.rvdxk.carrentalspringproject.entity.Rental;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentalRepository extends JpaRepository<Rental, Long> {
-}
-
