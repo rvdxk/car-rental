085d5d2b91f8bbfe4b8764cd39e342cd656026fe
rvdxk <radek.szczot@gmail.com>
2024-06-19 00:31:30 +0200

    car to cars on requestmapping

--- src/main/java/io/github/rvdxk/carrentalspringproject/controller/CarController.java
+++ src/main/java/io/github/rvdxk/carrentalspringproject/controller/CarController.java
@@ -16,19 +16,20 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/cars")
 public class CarController {
 
     @Autowired
     public CarService carService;
 
-    @PostMapping("/car/add")
+    @PostMapping("/add")
     @ResponseStatus(HttpStatus.CREATED)
     public String addCar(@RequestBody @Valid CarDto carDto){
         carService.addCar(carDto);
         return "Car successfully added!";
     }
 
-    @PostMapping("/car/{id}/parameters")
+    @PostMapping("/{id}/parameters")
     @ResponseStatus(HttpStatus.CREATED)
     public String addCarParameters(@PathVariable("id") Long id,
                                    @RequestBody @Valid CarParams carParams){
@@ -36,7 +37,7 @@ public class CarController {
         return "Car parameters successfully added!";
     }
 
-    @PostMapping("/car/{id}/car-location")
+    @PostMapping("/{id}/car-location")
     @ResponseStatus(HttpStatus.CREATED)
     public String addCarLocation(@RequestBody @Valid CarLocation carLocation,
                                  @PathVariable Long id){
@@ -44,54 +45,54 @@ public class CarController {
         return "Car location successfully added!";
     }
 
-    @GetMapping("/car")
+    @GetMapping
     public ResponseEntity<List<Car>> getAllCars(){
         List<Car> carsList = carService.getAllCars();
         return new ResponseEntity<>(carsList, HttpStatus.OK);
     }
 
 
-    @GetMapping("/car/{id}")
+    @GetMapping("/{id}")
     public CarDto getCarById(@PathVariable("id") Long id){
         CarDto carDto = carService.getCarById(id);
         return carDto;
     }
 
 
-    @GetMapping("/car/{id}/car-location")
+    @GetMapping("/{id}/car-location")
     public CarLocation getCarLocationById(@PathVariable("id") Long id){
         return carService.getCarLocationById(id);
     }
 
 
-    @PutMapping("/car/{id}")
+    @PutMapping("/{id}")
     public String updateCar(@PathVariable("id") Long id,
                             @RequestBody @Valid CarDto carDto){
         carService.updateCar(id, carDto);
         return "Car successfully updated!";
     }
 
-    @PutMapping("/car/{id}/parameters")
+    @PutMapping("/{id}/parameters")
     public String updateCarParams(@PathVariable(value = "id", required = true) Long id,
                                   @RequestBody @Valid CarParams carParams){
         carService.updateCarParam(id, carParams);
         return "Car parameters successfully updated!";
     }
 
-    @PutMapping("/car/{id}/car-location")
+    @PutMapping("/{id}/car-location")
     public String updateCarLocation(@PathVariable("id") Long id,
                                     @RequestBody @Valid CarLocation carLocation){
         carService.updateCarLocation(carLocation, id);
         return "Car location successfully updated!";
     }
 
-    @DeleteMapping("/car/{id}")
+    @DeleteMapping("/{id}")
     public String deleteCarById(@PathVariable("id") Long id){
         carService.deleteCar(id);
         return "Car successfully deleted!";
     }
 
-    @DeleteMapping("/car/{id}/car-location")
+    @DeleteMapping("/{id}/car-location")
     public String deleteCarLocation(@PathVariable("id") Long id){
         carService.deleteCarLocation(id);
         return "Car location successfully deleted!";
