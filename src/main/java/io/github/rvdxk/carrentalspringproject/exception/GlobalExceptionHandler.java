8fde2a1e307fe6ac705c441852f6aa9576f7c974
rvdxk <radek.szczot@gmail.com>
2024-06-27 01:57:11 +0200

    added exception

--- src/main/java/io/github/rvdxk/carrentalspringproject/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/rvdxk/carrentalspringproject/exception/GlobalExceptionHandler.java
@@ -3,9 +3,15 @@ package io.github.rvdxk.carrentalspringproject.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,5 +26,19 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException ex) {
         String message = "Access Denied: " + ex.getMessage();
         return new ResponseEntity<>(message, HttpStatus.FORBIDDEN);
+    }
+
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
